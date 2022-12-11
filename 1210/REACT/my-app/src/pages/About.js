a5a1019d3ea6552dc7d2a9e78790415b6e84ce56
hunsang <gjstkd123@naver.com>
2022-12-11 19:38:01 +0900

    1210

---
+++ 1210/REACT/my-app/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function About() {
+//     return <h1> Home 화면 입니다.</h1>;
+// }
+const About = () => {
+    return <h1> About 화면 입니다.</h1>;
+}
+
+export default About;
---
+++ 1210/REACT/my-app/src/pages/Home.js
@@ -0,0 +1,27 @@
+// 첫번째
+// import React from "react";
+
+// function Home() {
+//     return <h1> Home 화면 입니다.</h1>;
+// }
+
+// export default Home;
+
+// 두번째
+// import React, {Component} from "react";
+
+// class Home extends Component {
+//     render() {
+//         return <h1>Home 화면 입니다.</h1>;
+//     }
+// }
+
+// export default Home;
+
+import React from "react";
+
+const Home = () => {
+    return <h1> Home 화면 입니다.</h1>;
+}
+
+export default Home;
