617a1a467f87e928161422436ef651ea5ca55680
hunsang <gjstkd123@naver.com>
2023-01-26 17:53:28 +0900

    valid

--- 0126/muilearn/src/App.js
+++ 0126/muilearn/src/App.js
@@ -9,7 +9,7 @@ function App() {
   
   
   const [text, setText] = useState('');
-  const [error, setError] = useState(["중복된 닉네임입니다.", "글자수가 초과되었습니다.", "특수문자는 사용할 수 없습니다.", '자음, 모음만 사용할 수 없습니다.' ]);
+  const [error, setError] = useState(["중복된 닉네임입니다.", "글자수가 초과되었습니다.", "특수문자는 사용할 수 없습니다.", '자음, 모음만 사용할 수 없습니다.', '공백은 사용할 수 없습니다.' ]);
   
   
   const changeText = (e) => {
@@ -19,20 +19,23 @@ function App() {
 
     
     const validation = () => {
+      let check_alp = /^[a-zA-Z0-9]*$/;
       let check_spe = /[~!@#$%^&*()_+-/|<>?:{}.,/;='"\\\[\]`]/;
-      let check_str = /([^가-힣\x20])/i;
+      let check_str = /([^가-힣\x20])/;
+      // let space = /\s/i;
       // return check.test(text);
-      if (check_spe.test(text) === true) {
+      if (check_alp.test(text) === false) {
+        return ""
+      } else if (check_spe.test(text) === true) {
         return error[2]
+      } else if (check_str.test(text) === true) {
+        return error[3]
       } 
-      // else if (check_str.text(text) === true) {
-      //   return error[3]
-      // }
     }
 
   return (
     <div className="App">
-        <div>
+        <div className='nicknameInput'>
           <TextField
             id="nickname"
             placeholder="닉네임을 입력해주세요"
@@ -40,14 +43,15 @@ function App() {
             onChange={ changeText }
             value = { text }
             error = { validation() }
-            helperText= {validation() ? error[2]:""}
+            helperText= {validation() ? validation():""}
             
             // label="Helper text"
             // helperText="Some important text"
             />
-            {/* <input onChange={changeText} value={ text }/> */}
+          <div>
+            1
+          </div>
         </div>
-        
 
     </div>
   );
