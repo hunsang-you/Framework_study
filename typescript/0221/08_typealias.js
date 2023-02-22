03dcfebf6234123bcd69e1745bac46d1eb7a9124
hunsang <gjstkd123@naver.com>
2023-02-22 09:23:07 +0900

    0221

--- typescript/0221/08_typealias.js
+++ typescript/0221/08_typealias.js
@@ -13,3 +13,17 @@ let MemberInfo = {
 };
 MemberInfo.plusOne(1);
 MemberInfo.changeName();
+let cutZero = function (x) {
+    let result = x.replace(/^0+/, "");
+    return result;
+};
+function removeDash(x) {
+    let result = x.replace(/-/g, "");
+    return parseFloat(result);
+}
+function func5(x, func1, func2) {
+    let result = func1(x);
+    let result2 = func2(result);
+    console.log(result2);
+}
+func5("010-1111-2222", cutZero, removeDash);
--- typescript/0221/08_typealias.ts
+++ typescript/0221/08_typealias.ts
@@ -24,3 +24,25 @@ type Member = {
   plusOne: (x: number) => number;
   changeName: () => void;
 };
+
+type CutType = (x: string) => string;
+
+let cutZero: CutType = function (x) {
+  let result = x.replace(/^0+/, "");
+  return result;
+};
+
+function removeDash(x: string): number {
+  let result = x.replace(/-/g, "");
+  return parseFloat(result);
+}
+
+type tp1 = (x: string) => string;
+type tp2 = (x: string) => number;
+
+function func5(x, func1: tp1, func2: tp2) {
+  let result = func1(x);
+  let result2 = func2(result);
+  console.log(result2);
+}
+func5("010-1111-2222", cutZero, removeDash);
